c6ee65256b928de005d6558bc3f012f268e4f00d
Terry Ho <101287576+chung900811@users.noreply.github.com>
2022-08-13 22:25:08 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-const { fetchMyIP } = require('./iss')
-const { fetchCoordsByIP } = require('./iss')
-const { fetchFlyOver } = require('./iss')
-const { nextISSTimesForMyLocation } = require('./iss');
+const fetchMyIP = require('./iss');
+const fetchCoordsByIP = require('./iss')
+const fetchISSFlyOverTimes = require('./iss')
+const nextISSTimesForMyLocation = require('./iss')
 
-const printPassTimes = function(passTimes) {
-  for (const pass of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-};
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
 
-nextISSTimesForMyLocation((error, passTimes) => {
-  if (error) {
-    return console.log("It didn't work!", error);
-  }
-  // success, print out the deets!
-  printPassTimes(passTimes);
-});
+// let ip = "24.85.12.11";
+// fetchCoordsByIP(ip, (error, location) => {
+//   if (error) {
+//     console.log("It didn't work!" , error)
+//     return;
+//   } 
+//   console.log(location);
+// })
+
+  // let lat = { latitude: 49.2827291, longitude: -123.1207375 }
+  // fetchISSFlyOverTimes (lat, (error, passes) => {
+  //   if (error) {
+  //     console.log("It didn't work!" , error);
+  //     return;
+  //   } console.log(passes)
+  // })
 
 
+  nextISSTimesForMyLocation((error, passTimes) => {
+    if (error) {
+      return console.log("It didn't work!", error);
+    }
+      for (const pass of passTimes) {
+        const datetime = new Date(0);
+        datetime.setUTCSeconds(pass.risetime);
+        const duration = pass.duration;
+        console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+      }
+  });
